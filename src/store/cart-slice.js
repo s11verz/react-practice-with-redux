5fa6c7e70561a7e60813e8e6178d1eee9f4fd5ef
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-21 14:36:57 +0900

    Thunk, 데이터 가져오기

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 0,
-    totalAmount: 0,
+      totalQuantity: 0,
+      changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -18,11 +23,11 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.name,
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        existingItem.totalPrice += newItem.price;
       }
     },
     removeItemFromCart(state, action) {
@@ -33,10 +38,11 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
+
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
