31745d0050c8412fde7f50a5aa355f1208a9f634
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-30 13:45:02 +0900

    커스텀 훅 스토어 최적화

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import configureStore from "./hooks-store/products-store";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+configureStore();
 
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
